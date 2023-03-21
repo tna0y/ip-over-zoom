823c32d436b50f1672b15c9af97d35bc60ba6d37
Maxim Kochukov <kochukov.ma@phystech.edu>
2023-03-22 02:11:52 +0400

    clang-format

--- ip-over-zoom/cli/main.cpp
+++ ip-over-zoom/cli/main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
+
 #include "testlib.hpp"
 
 using namespace std;
 
-int main() {
-
+int main()
+{
   int a[10];
   a[10] = 1;
   test::testlib tdSSSS_SDDsdjonadoandadouadadad{};
-      tdSSSS_SDDsdjonadoandadouadadad.saySomething("yo!");
+  tdSSSS_SDDsdjonadoandadouadadad.saySomething("yo!");
   return 0;
 }
